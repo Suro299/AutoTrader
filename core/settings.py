715137324ec93f7a0f0bfeb479349c60baac5df0
suro299 <surendallakyan270407@gmail.com>
2023-06-22 01:13:14 +0400

    Test Commit

--- core/settings.py
+++ core/settings.py
@@ -1,14 +1,9 @@
 from pathlib import Path
 from . import config
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 AUTH_USER_MODEL = "users.CustomUser"
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config.DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
